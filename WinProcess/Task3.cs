d9d1bb8ffe15025d198f1239b9b5cffd09fff2a8
Arbalest <Evil_xacker@mail.ru>
2022-02-13 13:31:03 +0400

    Исправил запуск программы по относительному пути к 3 заданию.

--- WinProcess/Task3.cs
+++ WinProcess/Task3.cs
@@ -1,34 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WinProcess
 {
     public class Task3
     {
-        
-
-        public static string Run(string?[] args)
+        public static void Run(string?[] args)
         {
-            string _filePath = @"C:\Users\User\source\repos\WinProcess\CalculatorTask3\bin\Debug\net5.0\CalculatorTask3.exe";
-            var arguments = args?.Aggregate((prev, current) => prev + " " + current);
-            using var process = new Process
+            try
             {
-                StartInfo = new ProcessStartInfo
+                string _filePath = $"..\\..\\..\\..\\CalculatorTask3\\bin\\Debug\\net5.0\\CalculatorTask3.exe";
+                var arguments = args?.Aggregate((prev, current) => prev + " " + current);
+                using var process = new Process
                 {
-                    FileName = _filePath,
-                    Arguments = arguments ?? ""
-                }
-            };
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = _filePath,
+                        Arguments = arguments ?? ""
+                    }
+                };
 
-            process.Start();
+                process.Start();
 
-            while (!process.WaitForExit(1000)) { }
+                while (!process.WaitForExit(1000)) { }
 
-            return process.ExitCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
