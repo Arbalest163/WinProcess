fca21bb5688e0bb02f121953ac1754d893a8e5e9
Arbalest <Evil_xacker@mail.ru>
2022-02-13 13:37:55 +0400

    Добавлена блокировка кнопок при запуске задания

--- WinProcess/Form1.cs
+++ WinProcess/Form1.cs
@@ -23,12 +23,15 @@ namespace WinProcess
 
         private void button1_Click(object sender, EventArgs e)
         {
+            button1.Enabled = false;
             label2.Text = "Дочерний процесс запущен";
             label2.Text = Task1.Run();
+            button1.Enabled = true;
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
+            button2.Enabled = false;
             label3.Text = "Дочерний процесс запущен";
             label3.Text = await Task2.Run();
         }
@@ -36,10 +39,12 @@ namespace WinProcess
         private void button5_Click(object sender, EventArgs e)
         {
             Task2.Kill();
+            button2.Enabled = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            button3.Enabled = false;
             double value1 = 0;
             double value2 = 0;
             bool isNumber = Double.TryParse(textBox1.Text, out value1) && Double.TryParse(textBox2.Text, out value2);
@@ -52,6 +57,7 @@ namespace WinProcess
             var action = comboBox1.SelectedItem.ToString();
 
             Task3.Run(new[] {value1.ToString(), value2.ToString(), action });
+            button3.Enabled = true;
         }
     }
 }
